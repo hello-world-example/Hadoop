cd1e11d2f271ddd308b0c5befe17822d8b2d2fec
杨凯彬 <ykb553@163.com>
2018-03-20 22:35:29 +0800

    HFile

--- hbase-core/src/main/java/xyz/kail/demo/hbase/core/Main.java
+++ hbase-core/src/main/java/xyz/kail/demo/hbase/core/Main.java
@@ -1,29 +1,37 @@
 package xyz.kail.demo.hbase.core;
 
 
-import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.io.hfile.HFilePrettyPrinter;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by kail on 2018/3/5.
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
-        System.out.println(System.nanoTime());
+    public static void main(String[] args) throws Exception {
 
-        System.out.println(Long.toString(Long.MAX_VALUE));
+        List<String> params = Arrays.asList(
+                "-v",
+                "-e",
+                "-f", "/hbase/data/default/test/6ade7062cf7a8893ff685e5a8fa4d587/f/d6794bdd1094458eb71084502545d4fa"
+        );
+
+        args = params.toArray(new String[params.size()]);
+
+
+        Configuration conf = HBaseConfiguration.create();
+
+        conf.set(HConstants.HBASE_DIR, "hdfs://localhost:9000/hbase");
+
+        int ret = ToolRunner.run(conf, new HFilePrettyPrinter(), args);
+        System.exit(ret);
 
     }
 
